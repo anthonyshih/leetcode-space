98d9c58e1b4b932859a877c1d1b4dd923867b686
Hottari <110649618+Hottari@users.noreply.github.com>
2023-08-23 23:07:13 +0800

    338 comment
    add some comments of 338

--- main/dynamic_programming/338. Counting Bits.py
+++ main/dynamic_programming/338. Counting Bits.py
@@ -38,6 +38,33 @@ It is very easy to come up with a solution with a runtime of O(n log n). Can you
 Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
 '''
 
+
+'''
+Solution from Hottari
+
+Sol. I
+1. For each x belonging to 2^m, there must be a '1' (1, 10, 100, 10000...)
+2. Any y from 0 to n can be decomposed into 2^m + p.
+
+So, we can create a temporary integer called 'update' 
+and double it when i == update*2 (since 2^m must have a '1')
+
+Step 0: ans[0] = 0
+Step 1: Check 'update' (Is i == update*2?)
+Step 2: ans[i] = 1 + ans[i - update] (any y from 0 to n can be decomposed into 2^m + p)
+
+
+
+Sol. II
+1. The binary representation of an even number ends in xxxxx0, while that of an odd number ends in xxxxx1
+2. The fundamental method to find a binary representation is division by 2. ( "/2" ) 
+
+so,
+step 0: ans[0] = 0
+step 1: ans[i] = ans[int(i/2)] + i%2
+
+'''
+
 class Solution(object):
     def countBits(self, n):
         """
