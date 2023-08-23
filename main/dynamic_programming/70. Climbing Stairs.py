219121458d660c6c7915002465f4074e3fe3c438
WilliamChou <31893928+william60902@users.noreply.github.com>
2023-08-23 22:53:48 +0800

    Update 70. Climbing Stairs.py

--- main/dynamic_programming/70. Climbing Stairs.py
+++ main/dynamic_programming/70. Climbing Stairs.py
@@ -22,6 +22,24 @@ Explanation: There are three ways to climb to the top.
 1. 1 step + 1 step + 1 step
 2. 1 step + 2 steps
 3. 2 steps + 1 step
+
+
+note from William
+
+when you draw the decision tree
+you will find that this is a DP question
+try to do the buttom-up trace back 
+in the below is the case that n=5
+
+   _ _ _ _ _ _
+   8 5 3 2 1 1 
+
+so the key is to define the last one and two
+the keep rolling to the left 
+and you can notice this is a fib
+in this case is n-1
+we can keep updating the array till the end of the array
+
  
 """
 
@@ -31,3 +49,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+
+        one, two = 1, 1
+
+        for i in range(n-1):
+            temp = one # you save this with tme so you don't miss it
+            one = one + two
+            two = temp #then you got the two in temp and going going
+        
+        return one
